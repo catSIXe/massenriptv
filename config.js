e664990d6db433c93ce6e45243ed751f5f648668
cat6e <63829136+catSIXe@users.noreply.github.com>
2020-07-28 15:44:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    //cookies: `__cfduid=d354b6b54c7e39974c069920150840bf41587339155; __stripe_mid=cef0d7bc-213b-4ab5-9dad-f0783fd56f98; _mgtvRS=5e9cdfd59ebcd94fc30cc982-5coVDAFF6fO%2Fq42%2FeXUYIJgb; _mgtvSession=5e9cdfd59ebcd94fc30cc982%3A1c322ea79af4d8fd31cbf9dd225310aa; _mgtvSecSession=LXiitOF81Po1yKrs`,
+    //cookies: ``,
     //useragent: `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36`
     username: 'max.mustermann--at--domain.de',
     password: 'MASSENGESCHMACK TV ACCOUNT PASSWORD',
